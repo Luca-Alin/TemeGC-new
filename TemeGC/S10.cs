0cd09dcbc844fe9bb408242c5bf849297e819f19
Computer9088 <alinluca20021@gmail.com>
2023-05-29 06:10:22 +0300

    For a certain reason it works

--- TemeGC/S10.cs
+++ TemeGC/S10.cs
@@ -6,32 +6,22 @@
         private static List<PointF> _points = null!;
         private static List<Segment> _segments = null!;
 
-        private static PictureBox pb;
-        private static int kappa = 0;
+        private static PictureBox _pb = null!;
 
-        public static PictureBox P1(PictureBox picbox)
+
+        public static PictureBox P1(PictureBox pictureBox)
         {
-            pb = picbox;
-            pb.Size = new Size(Form1.width, Form1.height);
-            _g = pb.CreateGraphics();
+            _pb = pictureBox;
+            _pb.Size = new Size(Form1.width, Form1.height);
+            _pb.MouseClick += Pb_MouseClick;
+            _pb.MouseDoubleClick += Pb_MouseDoubleClick;
+
+            _g = _pb.CreateGraphics();
             _points = new List<PointF>();
             _segments = new List<Segment>();
 
-            for (int i = 0; i < 10; i++)
-            {
-                pain();
-                kappa++;
-            }
-            
-            return pb;
-        }
 
-
-        public static void pain()
-        {
-            pb.Image = null;
-            pb.Update();
-            _g.FillEllipse(new SolidBrush(Color.Red), 20 * kappa, 400, 10, 10);            
+            return _pb;
         }
 
 
@@ -44,6 +34,20 @@
                 pictureBox.MouseClick -= Pb_MouseDoubleClick;
             }
 
+            for (int i = 0; i < _points.Count; i++)
+                _segments.Add(
+                    new Segment(
+                        _points[i],
+                        _points[(i + 1 + _points.Count) % _points.Count]
+                    )
+                );
+
+            foreach (Segment segment in _segments)
+                DrawSegment(segment, Color.Black);
+                
+            Triangulate(_points);
+            
+            
             _points.Clear();
             _segments.Clear();
         }
@@ -56,6 +60,113 @@
             DrawPoint(_points[i], i);
         }
         //Mouse click handlers #end
+
+
+        //Additional functions #start
+        public static void Triangulate(List<PointF> points)
+        {
+            List<int> monotoneChainA = new List<int>();
+            List<int> monotoneChainB = new List<int>();
+            List<int> HT = new List<int>();
+            for (int i = 0; i < points.Count; i++)
+                HT.Add(i);
+            Console.Write(HT.Count + ": ");
+            HT.ForEach(n => Console.Write(n + " "));
+            Console.WriteLine();
+            
+            
+            int lowestPointIndex = 0, highestPointIndex = 0;
+            PointF lowestPoint = points[0], highestPoint = points[0];
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (points[i].Y > lowestPoint.Y)
+                {
+                    lowestPoint = points[i];
+                    lowestPointIndex = i;
+                }
+                else if (points[i].Y < highestPoint.Y)
+                {
+                    highestPoint = points[i];
+                    highestPointIndex = i;
+                }
+            }
+
+
+            for (int i = highestPointIndex; i <= lowestPointIndex; i++)
+                monotoneChainA.Add(i);
+            
+
+            for (int i = lowestPointIndex; i <= points.Count + highestPointIndex; i++)
+                monotoneChainB.Add((i + points.Count) % points.Count);
+
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    if (points[i].Y > points[j].Y)
+                    {
+                        (HT[i], HT[j]) = (HT[j], HT[i]);
+                    }
+
+                }
+            }
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+            stack.Push(1);
+
+            Console.Write(HT.Count + ": ");
+            HT.ForEach(n => Console.Write(n + " "));
+
+
+            Console.WriteLine();
+            
+            
+            
+            for (int i = 3; i < points.Count - 1; i++)
+            {
+                foreach (var i1 in stack)
+                {
+                    Console.Write(i1 + " ");
+                }
+
+                Console.WriteLine();
+                if (monotoneChainA.Contains(stack.Peek()) && monotoneChainB.Contains(HT[i]) ||
+                    monotoneChainA.Contains(HT[i]) && monotoneChainB.Contains(stack.Peek()))
+                {
+
+                    int aux = stack.Peek();
+                    while (stack.Count > 1)
+                    {
+                        int kappa = stack.Pop();
+                        DrawSegment(
+                            new Segment(points[kappa], points[HT[i]]), Color.Pink    
+                        );
+                    }
+                    stack.Pop();
+                    stack.Push(aux);
+                    stack.Push(HT[i]);
+                }
+                else
+                    stack.Push(HT[i]);
+                
+            }
+
+
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                DrawPoint(points[HT[i]], HT[i]);
+            }
+            
+            
+        }
+        
+        
+        
+        //Additional functions #end
 
 
         //Additional Classes #start
